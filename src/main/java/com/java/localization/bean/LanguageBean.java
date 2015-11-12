95ec8278cf89b456d7e5ce81d984dac59dad6337
kozmonaut <kozmonaut@example.com>
2015-11-12 20:21:30 +0100

    Initial commit

---
+++ src/main/java/com/java/localization/bean/LanguageBean.java
@@ -0,0 +1,28 @@
+package com.java.localization.bean;
+
+import java.io.Serializable;
+import java.util.Locale;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+ 
+@ManagedBean(name="language")
+@SessionScoped
+public class LanguageBean implements Serializable{
+	
+    private static final long serialVersionUID = 1L;
+    private Locale locale = FacesContext.getCurrentInstance().getViewRoot().getLocale();
+
+    public Locale getLocale() {
+        return locale;
+    }
+
+    public String getLanguage() {
+        return locale.getLanguage();
+    }
+
+    public void changeLanguage(String language) {
+        locale = new Locale(language);
+        FacesContext.getCurrentInstance().getViewRoot().setLocale(new Locale(language));
+    }
+}
